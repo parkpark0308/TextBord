27eca12134dc5e6e0ca183711801301ae9e07353
parkpark0308 <parkyk538@gmail.com>
2022-08-22 17:23:03 +0900

    uriparser 와 request 구현

---
+++ src/infra/Request.java
@@ -0,0 +1,32 @@
+package infra;
+
+import utils.UriParser;
+
+import java.util.Map;
+
+public class Request {
+
+    private UriParser uriParser;
+
+    public Request(String uri){
+        this.uriParser = new UriParser(uri);
+    }
+
+    public boolean isValidRequest(){
+        return false;
+    }
+
+    public String getControllerCode(){
+        return uriParser.getControllerCode();
+    }
+
+    public String getTarget(){
+        return uriParser.getTarget();
+    }
+
+    public Object getParaneterValue(String key,Class cls){
+        Map<String  , Object> parameter = uriParser.getParameter();
+        return cls.cast(parameter.get(key));
+    }
+
+}
